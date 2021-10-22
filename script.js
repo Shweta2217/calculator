7b33a3fe496bda1d581450612e3a8194aca3e9fe
Shweta <shwetasaini2217@gmail.com>
2021-10-22 16:40:18 +0530

    Minor changes and Some new functionalities added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const backSpace = document.getElementById('backSpace');
 const equal = document.getElementById('equal');
 const digits = document.querySelectorAll('.digit');
 
+//Click Event On every Button
 for (var i = 0; i < digits.length; i++) {
     document.querySelectorAll('.digit')[i].addEventListener("click", function() {
 
@@ -12,19 +13,24 @@ for (var i = 0; i < digits.length; i++) {
             innerText_ = "*";
         }
         console.log(innerText_);
+        document.querySelector(".warningText").style.visibility = "hidden";
         screen.value = (screen.value + innerText_);
     });
 }
 
+//Click  Eventlistner on Clear btn
 clear.addEventListener("click", function() {
     var empty = "";
     screen.value = empty;
 });
 
+//Click  Eventlistner on BackSpace
 backSpace.addEventListener("click", function() {
     var lenght = screen.value.length;
     screen.value = screen.value.substr(0, lenght - 1);
 });
+
+//Click  Eventlistner on =  
 equal.addEventListener("click", function() {
     var val = screen.value;
     val = val.trim();
@@ -32,24 +38,35 @@ equal.addEventListener("click", function() {
 
     if (val == "") {
         screen.value = "";
-        document.querySelector(".warningText").style.display = "block";
+        document.querySelector(".warningText").style.visibility = "visible";
+    } else if (val[0] == "*" || val[0] == "/" || val[0] == "-" || val[0] == "+"||val[0] == "%") {
+        document.querySelector(".warningText").style.visibility = "visible";
+        document.querySelector(".warningText").style.width = "60%";
+        document.querySelector(".warningText").innerText = "! Please enter digits first.";
+        screen.value = "";
     } else {
         val = val.replace(/ /g, "");
         var calculation = eval(val);
         console.log(calculation);
         screen.value = calculation;
-        document.querySelector(".warningText").style.display = "none";
+        document.querySelector(".warningText").style.visibility = "hidden";
     }
 });
 
+// eventListner on Keypresss
 document.addEventListener("keypress", function(event) {
     console.log(event.key);
     var eventValue = event.key;
     screen.value = screen.value + eventValue;
     if (isNaN(eventValue)) {
-        document.querySelector(".warningText").style.display = "block";
+        document.querySelector(".warningText").style.visibility = "visible";
         document.querySelector(".warningText").style.width = "60%";
         document.querySelector(".warningText").innerText = "! Please enter a valid value.";
     } else
-        document.querySelector(".warningText").style.display = "none";
+        document.querySelector(".warningText").style.visibility = "hidden";
 });
+
+//OnInput Function
+function onInputFunction() {
+    document.querySelector(".warningText").style.visibility = "hidden";
+}
